a04caf99a295e044081861ffcd6baf5cc635171f
JLR - Fahazavana <rjlucienaina@gmail.com>
2024-06-29 00:33:15 +0200

    Fixed ngram, text geneneration

--- src/NgramModel.py
+++ src/NgramModel.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
+
 import numpy as np
+
 from src.utils import sort_dict
 
 
@@ -19,32 +21,33 @@ class NGModel:
             _tmp = defaultdict(int)
             with open(file_name, "r") as corpus:
                 for sentence in corpus:
-                    tokenized_sentence = self.tokenizer.charSentenceTokenizer(
-                        sentence, order
-                    )
+                    tokenized_sentence = self.tokenizer.charSentenceTokenizer(sentence, order)
                     for ngram in tokenized_sentence:
                         _tmp[ngram] += 1
-                        self.vocab.update(ngram)
             self.counters[order] = sort_dict(_tmp)
+        self.vocab = [t[0] for t in self.counters[1].keys()]
         self.vocab_size = len(self.vocab)
         print("DONE!")
 
-    def Prob(self, seq, order):
-        n = len(seq)
-        if n > order:
+    def Prob(self, seq, alpha=1):
+        order = len(seq)
+        if order > self.orders:
             raise RuntimeError("len(seq) must be less or equal to order")
-        elif n == 1:
-            return self.counters[n].get(seq, 0) / sum(self.counters[n].values())
+
+        if order == 1:
+            count = self.counters[order].get(seq, 0)
+            total_count = sum(self.counters[order].values())
+            prob = (count + alpha) / (total_count + alpha * self.vocab_size)
         else:
-            pw1t = self.counters[order].get(seq, 0)
-            pw1t_1 = self.counters[order - 1].get(seq[: order - 1], 0)
-            if pw1t_1 == 0:
-                return 0
-            return pw1t / pw1t_1
+            # For higher-order n-grams
+            count = self.counters[order].get(seq, 0)
+            prefix = seq[:-1]
+            prefix_count = self.counters[order - 1].get(prefix, 0)
+            prob = (count + alpha) / (prefix_count + alpha * self.vocab_size)
+        return prob
 
     def LogProb(self, sentence, alpha=1):
         logprob = 0
-        counts = 0
         for ngram in self.tokenizer.charSentenceTokenizer(sentence, self.orders):
             order = len(ngram)
             if order > self.orders:
@@ -54,29 +57,43 @@ class NGModel:
                 # For unigrams
                 count = self.counters[order].get(ngram, 0)
                 total_count = sum(self.counters[order].values())
-                prob = (count + alpha) / \
-                    (total_count + alpha * self.vocab_size)
+                prob = (count + alpha) / (total_count + alpha * self.vocab_size)
             else:
                 # For higher-order n-grams
                 count = self.counters[order].get(ngram, 0)
                 prefix = ngram[:-1]
                 prefix_count = self.counters[order - 1].get(prefix, 0)
-                prob = (count + alpha) / \
-                    (prefix_count + alpha * self.vocab_size)
+                prob = (count + alpha) / (prefix_count + alpha * self.vocab_size)
 
             if prob > 0:
                 logprob += np.log(prob)
             else:
                 logprob += -np.inf
-            counts += 1
-        return logprob, counts
+        return logprob
 
     def perplexity(self, sentence, alpha=1, doc=False):
-        """
-        """
         T = len(sentence.strip()) + 2  # +2 for start and end tokens
-        logprob = self.LogProb(sentence, alpha)[0]
+        logprob = self.LogProb(sentence, alpha)
         if doc:
             return logprob, T
-        else:
-            return np.exp(-logprob / T)
+        return np.exp(-logprob / T)
+
+    def __nextChar(self, context, alpha=1):
+        probs = np.zeros(self.vocab_size)
+        for i in range(len(self.vocab)):
+            ngram = context + [self.vocab[i]]
+            probs[i] = self.Prob(tuple(ngram), alpha=1)
+        probs /= np.sum(probs)
+        chr_idx = np.random.multinomial(1, probs).argmax()
+        return self.vocab[chr_idx]
+
+    def generateSentence(self, start=None, order=None, alpha=1):
+        if order is None:
+            order = self.orders
+        sentence = [self.tokenizer.start]
+        if start is not None:
+            sentence += list(start)
+        while sentence[-1] != self.tokenizer.end:
+            context = sentence[-(order - 1):]
+            sentence.append(self.__nextChar(context))
+        return "".join(sentence)
