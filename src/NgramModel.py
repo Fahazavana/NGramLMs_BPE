411f34d7cf70cefb97abe9dba9dc3f3830d1f131
JLR - Fahazavana <rjlucienaina@gmail.com>
2024-06-30 17:52:20 +0200

    Update references, arrange language order

--- src/NgramModel.py
+++ src/NgramModel.py
@@ -15,7 +15,7 @@ class NGModel:
         - Count all N-gram
         - Probability and LogProbability computing
         - Perplexity computing
-        - Smooting Methods: unsmoothed, Laplace, Add-alpha
+        - Smooting Methods: unsmoothed, Laplace, Add-k
     """
 
     def __init__(self, name: str, orders: int, tokenizer: Tokenizer) -> None:
@@ -70,29 +70,29 @@ class NGModel:
             den = self.counters[order - 1].get(ngram[:-1], 0)
         return (num, den)
 
-    def prob(self, ngram: Tuple, alpha: int = 1) -> float:
+    def prob(self, ngram: Tuple, k: float = 1.0) -> float:
         """
             Compute the probability of a N-gram using:
-            - alpha = 0: Unsmoothed
-            - alpha = 1: Laplace
-            - 1<alpha <0: add-alpha
+            - k = 0: Unsmoothed
+            - k = 1: Laplace
+            - 1< k <0: add-k
         Args:
             ngram: The N-gram
-            alpha: The smoothing parameter by default 1
+            k: The smoothing parameter by default 1
         Returns:
             (float): The N-gram probability
         """
         num, den = self.get_count(ngram)
-        prob = (num + alpha) / (den + alpha * self.vocab_size)
+        prob = (num + k) / (den + k * self.vocab_size)
         return prob
 
-    def probInt(self, ngram: Tuple[str], alphas: List) -> float:
+    def probInt(self, ngram: Tuple[str], lambdas: List) -> float:
         """
             Compute the probability of an n_gram
             usign interpolation
         Args:
             ngram: The N-gram
-            alphas: List of the interpoltion weight, the first one correspond to the highest N-gram order
+            lambdas: List of the interpoltion weight, the first one correspond to the highest N-gram order
             and the second one correspond to the next higher N-gram order, to the uni-gram order
         Returns:
             (float): The N-gram probability
@@ -102,44 +102,44 @@ class NGModel:
             ngram = ngram[-self.orders + i:]
             num, den = self.get_count(ngram)
             if num != 0:
-                prob += alphas[i] * (num / den)
+                prob += lambdas[i] * (num / den)
         return prob
 
-    def logProbInt(self, sentence: str, alphas: List[float]):
+    def logProbInt(self, sentence: str, lambdas: List[float]):
         """
             Compute the log probability of an input sentence
             using interpolation.
         Args:
             sentence: The input sentence
-            alphas: List of the interpolation weight, the first one correspond to the highest N-gram order
+            lambdas: List of the interpolation weight, the first one correspond to the highest N-gram order
         Returns:
             (float): The log probability of the input sentence
         """
         logprob, prob = 0, 0
         for ngram in self.tokenizer.charSentenceTokenizer(sentence, self.orders):
-            logprob += np.log(self.probInt(ngram, alphas))
+            logprob += np.log(self.probInt(ngram, lambdas))
         return logprob
 
-    def logProb(self, sentence, alpha=0):
+    def logProb(self, sentence:str, k:float=1.0)->float:
         """
-        Compute the log probability of an input sentence using the Add-alpha smoothing
+        Compute the log probability of an input sentence using the Add-k smoothing
         Args:
             sentence: input sentence
-            alpha: smoothing parameter, by default 1 (Laplace)
+            k: smoothing parameter, by default 1 (Laplace)
 
         Returns:
             (float): The log probability of the input sentence
         """
         logprob, prob = 0, 0
         for ngram in self.tokenizer.charSentenceTokenizer(sentence, self.orders):
-            prob = self.prob(ngram, alpha)
+            prob = self.prob(ngram, k)
             if prob > 0:
                 logprob += np.log(prob)
             else:
                 return -np.inf
         return logprob
 
-    def __perplexity(self, sentence, params: Dict, mode: str = 'default') -> Tuple[float, int]:
+    def __perplexity(self, sentence, params: Dict, mode: str = 'add_k') -> Tuple[float, int]:
         """
             Compute the logProbability of a Sentence according to the mode and parameters,
             and the lengnth of the sentence, these value will be used to compute the perplexity
@@ -147,29 +147,29 @@ class NGModel:
         Args:
             sentence: input sentence
             params: Dictionnary of parameters for the model
-            mode: Mode for computing the perplexity, by default 'default'
-                -default: Add-alpha smoothing -> the params must have the form:
-                        {'alpha': 0.1}
+            mode: Mode for computing the perplexity, by default 'add_k'
+                -default: Add-k smoothing -> the params must have the form:
+                        {'k': 0.1}
                 -inter: Interpolation smoothing -> the parameter must have the form:
-                {'alphas': [0.1, 0.5, 0.4]}
+                {'lambdas': [0.1, 0.5, 0.4]}
         Returns:
             (float,int): logProbability of the input sentence and its lenght.
         """
         T = len(sentence.strip()) + 2  # +2 for start and end tokens
         logprob = 0
-        if mode == "default":
-            logprob = self.logProb(sentence, params['alpha'])
+        if mode == "add_k":
+            logprob = self.logProb(sentence, params['k'])
         elif mode == "inter":
-            logprob = self.logProbInt(sentence, params['alphas'])
+            logprob = self.logProbInt(sentence, params['lambdas'])
         return logprob, T
 
-    def perplexity(self, sentence: str, params: Dict, mode='default', doc=False) -> Union[float, Tuple[float, int]]:
+    def perplexity(self, sentence: str, params: Dict, mode='add_k', doc=False) -> Union[float, Tuple[float, int]]:
         """
         Compute the perplexity of a sentence or the Logprobabilty of a Sentence and its length if we say doc=True
         Args:
             sentence: the input sentence
             params: Parameters for the model to compute the probabilty
-            mode: smoothing mode, by default 'default' (Add-alpha smoothing)
+            mode: smoothing mode, by default 'add_k' (Add-k smoothing)
             doc:
                 - False for a single sentecce -> perplexity
                 - True for as single sentence in adocuments -> logProbability, sentence lenght
@@ -185,13 +185,13 @@ class NGModel:
         else:
             return np.exp(-logprob / T)
 
-    def __nextChar(self, context: Tuple, params: Dict, mode='default') -> str:
+    def __nextChar(self, context: Tuple, params: Dict, mode='add_k') -> str:
         """
         Get the next character from the context, the contect is a bi-gram if we use a tri-gram model
         Args:
             context: the ngram context to generate the next character
             params: parameters for the model to compute the probabilty
-            mode: smmothing method to compute the probability, by default 'default'
+            mode: smoothing method to compute the probability, by default 'add_k'
 
         Returns:
             (str): next character
@@ -199,22 +199,22 @@ class NGModel:
         probs = np.zeros(self.vocab_size)
         for i in range(len(self.vocab)):
             ngram = context + (self.vocab[i],)
-            if mode == "default":
-                probs[i] = self.prob(ngram, params['alpha'])
+            if mode == 'add_k':
+                probs[i] = self.prob(ngram, params['k'])
             elif mode == "inter":
-                probs[i] = self.probInt(ngram, params['alphas'])
+                probs[i] = self.probInt(ngram, params['lambdas'])
         probs /= np.sum(probs)
         chr_idx = np.random.multinomial(1, probs).argmax()
         return self.vocab[chr_idx]
 
-    def generateSentence(self, params: Dict, start='', mode='default') -> str:
+    def generateSentence(self, params: Dict, start='', mode='add_k') -> str:
         """
         Generate a sentence based on parameters/mode and the starting text.
         The starting is splited into charater if not empty and add the sentence starting marker.
         Args:
             params: parameters for the model to compute the probabilty
             start: starting character or text to generate sentence
-            mode: smmothing method to compute the probability, by default 'default'
+            mode: smmothing method to compute the probability, by default 'add_k'
 
         Returns:
             (str): generated sentence
@@ -229,37 +229,37 @@ class NGModel:
         return "".join(sentence)
 
 
-def tune_alpha(model: NGModel, val_file: str, alphas: np.ndarray) -> List[float]:
+def tune_k(model: NGModel, val_file: str, k_list: np.ndarray) -> List[float]:
     """
-    Tune the smoothing parameters of the model with Add-alpha smoothing.
+    Tune the smoothing parameters of the model with Add-k smoothing.
     Args:
         model: Model to be tuned
         val_file: validation file path
-        alphas: list of alphas
+        k_list: list of k
 
     Returns:
-        (list): list of perplexities for each alpha considered
+        (list): list of perplexities for each k considered
     """
     perplexities = []
-    for alpha in alphas:
+    for k in k_list:
         sums, counts = 0, 0
         with open(val_file) as corpus:
             for line in corpus:
-                p, c = model.perplexity(line, params={'alpha': alpha}, doc=True, mode='default')
+                p, c = model.perplexity(line, params={'k': k}, doc=True, mode='add_k')
                 sums += p
                 counts += c
         perplexities.append(np.exp(-sums / counts))
     return perplexities
 
 
-def evalModels(models: Tuple[NGModel], val_file: str, params: Dict, mode: str = 'default') -> np.ndarray:
+def evalModels(models: Tuple[NGModel], val_file: str, params: Dict, mode: str = 'add_k') -> np.ndarray:
     """
     Evaluate the models with the given parameters/mode on with a validation set
     Args:
         models: list of models to be evaluated
         val_file: validation file path
         params: parameters for the model to compute the probabilty
-        mode: smooting mode, by default 'default'
+        mode: smooting mode, by default 'add_k'
 
     Returns:
         (numpy.ndarray): list of perplexities on each model
@@ -277,14 +277,14 @@ def evalModels(models: Tuple[NGModel], val_file: str, params: Dict, mode: str = 
     return docpp
 
 
-def make_cmf(models: Tuple[NGModel], val_files: Tuple[str], params: Dict, mode='default', title: str = '') -> None:
+def make_cmf(models: Tuple[NGModel], val_files: Tuple[str], params: Dict, mode='add_k', title: str = '') -> None:
     """
     Make a confusion matrix of all models with the given parameters/mode on a several validation set
     Args:
         models: List of models to be evaluated
         val_files: List of validation file paths to evaluate each model
         params: parameters for the model to compute the probabilty
-        mode: smooting mode, by default 'default'
+        mode: smooting mode, by default 'add_k'
         title: Title of the confusion matrix plot
 
     Returns:
