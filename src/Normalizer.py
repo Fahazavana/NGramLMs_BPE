e288b80d0717c616c726d7a82cee0ab310c5a541
JLR - Fahazavana <rjlucienaina@gmail.com>
2024-06-30 06:41:08 +0200

    Update README.md and comment on main.ipynb

--- src/Normalizer.py
+++ src/Normalizer.py
@@ -5,7 +5,9 @@ import unicodedata
 
 def paragraph_normalizer(text: str) -> str:
     """
-    Normalize a paragraph of text:
+    Normalize a paragraph of text.
+
+    The normalization process includes the following steps:
     - Remove leading/trailing whitespace
     - Normalize diacritics to simple characters
     - Expand abbreviations and acronyms by inserting spaces
@@ -20,25 +22,20 @@ def paragraph_normalizer(text: str) -> str:
     Returns:
         str: The normalized text.
     """
-    # Normalize diacritics to simple characters
-    text = unicodedata.normalize('NFKD', text).encode(
-        'ascii', 'ignore').decode('ascii')
 
-    # Expand abbreviations and acronyms
-    acrs_and_abvs = re.findall(
-        r"\b([A-Z]{2,}|[A-Z]+[0-9]+|[A-Z]+[a-z]+[A-Z]|[a-z]+[0-9]+)", text)
+    # Normalize diacritics to simple characters
+    text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('ascii')
+
+    acrs_and_abvs = re.findall(r"\b([A-Z]{2,}|[A-Z]+[0-9]+|[A-Z]+[a-z]+[A-Z]|[a-z]+[0-9]+)", text)
     for acr_and_abv in acrs_and_abvs:
         if any(char in string.punctuation for char in acr_and_abv) and len(acr_and_abv) > 1:
             text = text.replace(acr_and_abv, " ".join(acr_and_abv))
 
-    # Split text into sentences
     text = re.sub(r"([.!?]\s)(?=[A-Z])", "\n", text)
 
-    # Replace digits with '0'
     text = re.sub(r"\d", "0", text)
-
-    # Replace non-alphanumeric characters (except spaces and newlines) with a single space
     text = re.sub(r"[^\w\s\n]", " ", text)
+
     text = re.sub(r"\s{2,}", " ", text)
     text = re.sub(r"_", " ", text)
 
@@ -48,7 +45,7 @@ def paragraph_normalizer(text: str) -> str:
     return text
 
 
-def normalize_file(in_file: str, out_file: str):
+def normalize_data(in_file: str, out_file: str)->None:
     """
     Normalize the contents of a file and write the results to another file.
 
@@ -60,8 +57,7 @@ def normalize_file(in_file: str, out_file: str):
         with open(in_file, 'r') as file:
             with open(out_file, 'w') as normalized_file:
                 for line in file:
-                    normalized_file.write(
-                        paragraph_normalizer(line.strip()) + '\n')
+                    normalized_file.write(paragraph_normalizer(line.strip()) + '\n')
             print("Document Normalized Successfully!")
     except FileNotFoundError:
         print(f"File not found at {in_file}")
